21c8841aaca5ce793d70aa38d0638b0ab5674f24
Rainer Du <dusiyh@gmail.com>
2012-10-27 16:32:26 +0800

    add view of locker to admin

--- app/models/locker.rb
+++ app/models/locker.rb
@@ -1,4 +1,13 @@
 class Locker < ActiveRecord::Base
   attr_accessible :identifier
   validates_uniqueness :identifier
+  state_machine :status, :initial => :free do
+    event :rent do
+      transition :free => :occupied
+    end
+
+    event :return do
+      transition :occupied => :free
+    end
+  end
 end
