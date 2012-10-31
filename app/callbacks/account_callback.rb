7425fafd302abe9250c606217f00e24efe90abfa
Rainer Du <dusiyh@gmail.com>
2012-11-01 00:24:56 +0800

    add membership info for account

--- app/callbacks/account_callback.rb
+++ app/callbacks/account_callback.rb
@@ -1,14 +1,14 @@
 class AccountCallback < FatFreeCRM::Callback::Base
   def account_tools_before view, context
+    result = ""
     account = context[:account]
     if account.membership
       if account.expired?
-        view.content_tag(:li) do
+        result << view.content_tag(:li) do
           view.link_to_renewal(account)
         end
       end
       if account.normal?
-        result = ""
         result << view.content_tag(:li) do
           view.link_to_suspend(account)
         end
@@ -17,15 +17,25 @@ class AccountCallback < FatFreeCRM::Callback::Base
         end
       end
     else
-      view.content_tag(:li) do
+      result << view.content_tag(:li) do
         view.link_to_renewal(account)
       end
     end
+    result
   end
 
-  def account_button view, context
+  def account_bottom view, context
+    result = ""
     account = context[:account]
-    if account.membership
+    if membership = account.membership
+      result << membership.type.name + ":"
+      result << "#{membership.start_date.strftime("%Y/%m/%d")} - #{membership.due_date.strftime("%Y/%m/%d")}"
+      if membership.expired?
+        result << view.content_tag(:span, :class => "warning") do
+          view.t(:expired)
+        end
+      end
     end
+    result
   end
 end
