d9fa33ae9f058c70daad9d918c6045efc9e8f8fd
Rainer Du <dusiyh@gmail.com>
2012-11-11 05:44:59 +0800

    contract creation accept parameters

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -14,6 +14,7 @@ class ContractsController < ApplicationController
       if @template
         @contract = Contract.new
         @contract.contract_template = @template
+        @contract.parameters_attributes = params
       end
     end
   end
