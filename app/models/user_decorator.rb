e2fa300a160e00d3ae459bed8ea65aa3c61486a3
siyu.du <dusiyh@gmail.com>
2012-12-16 00:32:18 +0800

    add user_orders and add ordered scope to User

--- app/models/user_decorator.rb
+++ app/models/user_decorator.rb
@@ -21,6 +21,9 @@ User.class_eval do
 
   after_create :initialize_schedule
 
+
+  scope :ordered, includes(:user_order).order('COALESCE(user_orders.order, 999999) asc')
+
   scope :manageable_by, (lambda do |user|
     where("#{role_mask_column} & :role_mask > 0 or users.id = :user_id", { :role_mask => mask_for(*user.manageable_roles), :user_id => user.id})
   end)
