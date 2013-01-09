027c7f986c5374514db2f6b13985f9cbf368384c
siyu.du <dusiyh@gmail.com>
2013-01-09 13:05:31 +0800

    add confirmation when participate a lesson

--- app/helpers/participations_helper.rb
+++ app/helpers/participations_helper.rb
@@ -2,6 +2,7 @@ module ParticipationsHelper
   def link_to_attend participation
     link_to( t(:attend), attend_participation_path(participation),
              :method => :post,
+             :confirm => t("view.participation.attend_confirm"),
              :remote => true
     )
   end
