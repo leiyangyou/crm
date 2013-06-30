30ff94d81635a213c85ee4c610c364e7ee35c31b
siyu.du <dusiyh@gmail.com>
2013-06-30 17:51:30 +0800

    make trainer nullable when creating

--- app/helpers/users_helper_decorator.rb
+++ app/helpers/users_helper_decorator.rb
@@ -3,7 +3,7 @@ UsersHelper.class_eval do
     @users ||= User.active
   end
 
-  def scoped_users_select(asset, users, description_field = :full_description, method = :assigned_to, options = {})
-    collection_select asset, method, users, :id, description_field, {}, options
+  def scoped_users_select(asset, users, description_field = :full_description, method = :assigned_to, options = {}, html_options = {})
+    collection_select asset, method, users, :id, description_field, options, html_options
   end
 end
